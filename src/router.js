0c99768daf50ab3cc17bdb45dbe060b04b971596
Noah Gentile <noah.gentile@gmail.com>
2019-10-12 17:03:38 -0400

    Cleaning up unused views

--- src/router.js
+++ src/router.js
@@ -1,16 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Dashboard from './views/Dashboard.vue';
-import About from './views/About.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'about',
-      component: About,
-    },
-  ],
+  routes: [],
 });
